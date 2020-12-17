e5ddf5d27250d1aec9e028ed6c1ae2a601b405c1
Bumpy2k <thephilipchannelmain@gmail.com>
2020-12-16 20:44:15 -0500

    Addons

--- toontown/coghq/DistributedNameDropperCabnit.py
+++ toontown/coghq/DistributedNameDropperCabnit.py
@@ -13,11 +13,14 @@ from toontown.toonbase import ToontownGlobals
 from toontown.toonbase import TTLocalizer
 from toontown.battle import MovieUtil
 from direct.actor.Actor import Actor
+from direct.interval.LerpInterval import LerpColorInterval
+from direct.interval.LerpInterval import LerpScaleInterval
 
 class DistributedNameDropperCabnit(DistributedObject.DistributedObject):
     def __init__(self, cr):
         DistributedObject.DistributedObject.__init__(self, cr)
         #Load The Cabnit!
+        self.cr
         self.makeModel()
         
         self.accept("entercoll", self.giveReward)
@@ -27,10 +30,18 @@ class DistributedNameDropperCabnit(DistributedObject.DistributedObject):
     def makeModel(self):
         #Place Holder!
         
-        cabND = Actor('phase_5/models/cogdominium/tt_namedropper_cabnit_zero.bam')
-        cabND.reparentTo(render)
-        cabND.setPos(-20, -20, 0)
+        self.cabND = Actor('phase_5/models/cogdominium/tt_namedropper_cabnit_zero.bam')
+        self.cabND.reparentTo(render)
+        self.cabND.setPos(-20, -20, 0)
     
     def giveReward(self, entry):
         print('Hi! here you go!')
+        rewardMovie = LerpColorInterval(self.cabND, 0.3, Vec3(0, 0, 1), Vec3(1, 1, 1), blendType='easeInOut')
+        rewardMovie2 = LerpColorInterval(self.cabND, 0.3, Vec3(1, 1, 1), Vec3(0, 0, 1), blendType='easeInOut')
+        rewardScale = LerpScaleInterval(self.cabND, 0.3, Vec3(1, 1, 2), Vec3(1, 1, 1), blendType='easeInOut') 
+        rewardScale2 = LerpScaleInterval(self.cabND, 0.3, Vec3(1, 1, 1), Vec3(1, 1, 2), blendType='easeInOut')
+        gain = base.loader.loadSfx("phase_11/audio/sfx/LB_toonup.ogg")
+        gain.play()
+        rewardPlay = Sequence(rewardMovie, rewardMovie2).start()
+        rewardPlay = Sequence(rewardScale, rewardScale2).start()
         self.sendUpdate("giveReward", [])
